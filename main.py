0a8e9877af9f43c45e92cd31c37aecffe097b1f4
nipunarora8 <nipunarora8@yahoo.com>
2023-01-30 01:41:30 +0100

    Updated main, preprocessed data, plotly fix

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from mlts.models import LSTM
 from mlts.config import *
 import pandas as pd
+import argparse
 
 
-def run():
+def run(dataset):
     # Load the historical stock prices for AAPL
-    stock_data = pd.read_csv(
-        DatasetPath.APPLE.value, parse_dates=['date'],
-        index_col='date'
-    )
+    stock_data = pd.read_csv(DatasetPath[dataset].value, parse_dates=['date'],index_col='date')
     
     my_model = LSTM()
     my_model.fit(stock_data)
 
-
 if __name__ == '__main__':
-    run()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset') #APPL, GMBL, TSLA
+    args = parser.parse_args()
+    dataset = args.dataset
+    run(dataset)
